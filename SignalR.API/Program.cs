c0737e822acaf45107e91df11b0ec7b2375d419a
feyyaz başer <feyaz12@hotmail.com>
2023-05-31 17:42:40 +0300

    Client project added

--- SignalR.API/Program.cs
+++ SignalR.API/Program.cs
@@ -1,8 +1,11 @@
+using SignalR.API.Hubs;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddSignalR();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -20,6 +23,7 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
+app.MapHub<MyHub>("/MyHub");
 app.MapControllers();
 
 app.Run();
